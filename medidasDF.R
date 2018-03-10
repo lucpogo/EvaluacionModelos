0a7a8bc8f130d9cfd3fd2cfc528c1c56d1a03a27
Lucas Pogorelsky <ldpogorelsky@gmail.com>
2018-03-10 09:13:04 -0300

    Cambio de nombre de variable de df a ds

--- medidasDF.R
+++ medidasDF.R
@@ -1,18 +1,13 @@
 medidasDF <- function(real,predicted,positive=1,digitsRound=4){
   library(dplyr)
   library(tidyr)
-  df <- tibble(predicted=round(predicted,digits=digitsRound),
+  ds <- tibble(predicted=round(predicted,digits=digitsRound),
                real=ifelse(real==positive,'P','N'),
                stringsAsFactors = F) %>%
     group_by(real,predicted) %>%
     summarise(N=n()) %>%
     spread(real,N,fill=0) %>%
     arrange(desc(predicted)) %>%
-    #Para que el gráfico comience en (0,0)
-    #add_row(predicted=NA,
-    #        P=0,
-    #        N=0,
-    #        .before=1) %>%
     mutate(Tot=P+N,
            PAcum=cumsum(P),
            NAcum=cumsum(N),
@@ -22,8 +17,8 @@ medidasDF <- function(real,predicted,positive=1,digitsRound=4){
            Precision=PAcum/TotAcum,
            Accuracy=(PAcum+sum(N)-NAcum)/sum(Tot),
            F1Score=2*(Sens*Precision)/(Sens+Precision),
-           PropTot=round(TotAcum/sum(Tot),2)*100,
+           PropTot=TotAcum/sum(Tot),
            Lift=Precision/(sum(P)/sum(Tot))
     )
-  return(df)
+  return(ds)
 }
